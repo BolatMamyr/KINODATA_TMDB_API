6e4a4c06a2f7d30bc5c7d58a33a69d5c2cf5b0bc
bolat <bolatmamyr123@gmail.com>
2022-12-15 21:51:23 +0600

    SearchFragment is started

--- app/src/main/java/com/example/kinodata/model/multiSearch/multiSearch.kt
+++ app/src/main/java/com/example/kinodata/model/multiSearch/multiSearch.kt
@@ -2,7 +2,7 @@ package com.example.kinodata.model.multiSearch
 
 data class multiSearch(
     val page: Int,
-    val searchResults: List<SearchResult>,
+    val results: List<SearchResult>,
     val total_pages: Int,
     val total_results: Int
 )
